3c0da02c88659458e428f7d9b691dcc7615f0280
Bryan Duggan <skooter500@gmail.com>
2020-04-21 14:30:02 +0100

    bla

--- java/src/ie/tudublin/Gant.java
+++ java/src/ie/tudublin/Gant.java
@@ -1,12 +1,105 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
 import processing.core.PApplet;
+import processing.data.*;
 
-public class HelloProcessing extends PApplet
+public class Gant extends PApplet
 {	
+	ArrayList<Task> tasks = new ArrayList<Task>();
+	
 	public void settings()
 	{
-		size(500, 500);
+		size(800, 600);
+
+		loadTasks();
+		printTasks();
+	}
+
+	public void loadTasks()
+	{
+		Table table = loadTable("tasks.csv", "header");
+
+		for(TableRow row:table.rows())
+		{
+			Task task = new Task(row);
+			tasks.add(task);
+		}
+	}
+
+	public void printTasks()
+	{
+		for(Task t:tasks)
+		{
+			println(t);
+		}
+	}
+
+	private boolean isEnd = false;
+	private int whichTask = -1;
+
+	private float border = 40;
+	private float rowHeight = 40;
+	float namesPart = 150;
+
+	public void mousePressed()
+	{
+		for(int i = 0 ; i < tasks.size() ; i ++)
+		{
+			float y1 = (border + border + rowHeight * i) - 15;
+			float y2 = (border + border + rowHeight * i) + 20;
+
+			float x1 = map(tasks.get(i).getStart(), 1, 30, namesPart, width - border);
+			float x2 = map(tasks.get(i).getEnd(), 1, 30, namesPart, width - border);
+			
+			if (mouseX >= x1 && mouseX <= x2 && mouseY >= y1 && mouseY <= y2)
+			{
+				whichTask = i;
+				println(whichTask);
+				break;
+			}
+
+		}
+			
+	}
+
+	public void mouseDragged()
+	{
+
+	}
+
+	void displayTasks()
+	{
+		
+		textSize(14);
+		textAlign(LEFT,CENTER);
+		
+		
+		textAlign(CENTER, CENTER);
+		stroke(128);
+		for(int i = 1 ; i <= 30 ; i ++)
+		{
+			float x = map(i, 1, 30, namesPart, width - border);
+			line(x, border, x, height - border);
+			fill(255);
+			text(i, x, border * 0.5f);
+		}
+
+		textAlign(LEFT, CENTER);
+		for(int i = 0 ; i < tasks.size() ; i ++)
+		{
+			float y = border + border + rowHeight * i;
+			fill(255);
+			text(tasks.get(i).getName(), border, y);
+
+			// Print the task
+			noStroke();
+			fill(0, 255, 0);
+			float x1 = map(tasks.get(i).getStart(), 1, 30, namesPart, width - border);
+			float x2 = map(tasks.get(i).getEnd(), 1, 30, namesPart, width - border);
+			rect(x1, y - 15, x2 - x1, rowHeight - 5, 5.0f);
+		}
+
 	}
 
 	public void setup() {
@@ -29,12 +122,8 @@ public class HelloProcessing extends PApplet
 	int x = 0; // 4 bytes
 	
 	public void draw()
-	{	
-		
+	{			
 		background(0);
-		noStroke();
-		fill(255);
-		ellipse(x, mouseY, 50, 50);		
-		x ++;
+		displayTasks();
 	}
 }
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -3,124 +3,15 @@ package ie.tudublin;
 public class Main
 {
 
-    public void soundSynthesis()
+    public void gant()
 	{
 		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new SoundSynthesis());
-    }
-    public void starMap()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new StarMap());
-    }
-    public void helloArrays()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new ArraysExample());
-    }
-
-    public void helloProcessing()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new HelloProcessing());
-    }
-
-    public void loops()
-    {
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Loops());
-    }
-    
-    public void bugZap()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new BugZap());
-    }
-    
-    public void sound1()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Sound1());
-    }
-
-    public void sound2()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Sound2());
-    }
-
-    public void audioBands()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new AudioBands());
-    }
-
-    public void airSeaBattle()
-	{
-		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new AirSeaBattle());
-    }
-
-    public void soundMandala()
-    {
-        String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new SoundMandala());
-    }
-
-    public void threeD()
-    {
-        String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new ThreeD());
-    }
-
-    public void cafe()
-    {
-        String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Cafe());
-    }
-    
-    public void dogsCats()
-    {
-        // Polymorphism means
-        // The type is a superclass, the instance is a subclass
-
-        // This is not polymorphism
-        Cat topcat = new Cat("Topcat");
-
-        // This is polymorphism!
-        Animal mino = new Cat("Mino");
-        // A vitual method!
-        // Dynamic binding
-        mino.speak();
-        // This wont compile!!
-        //System.out.println(mino.getNumLives());
-        // This is an example of a cast
-        System.out.println(((Cat)mino).getNumLives());
-
-        mino = new Dog("Misty");  // This will compile!
-        //topcat = new Dog("Tara"); // This wont compile!
-
-        Animal a = new Cat("a");
-        Animal b = new Cat("b");
-        
-        System.out.println(a);
-        System.out.println(b);
-
-        a = b;
-
-        a.setName("c");
-
-        System.out.println(a);
-        System.out.println(b);
-
-        // c c will get printed out because 
-        // a & b are object references
-
+        processing.core.PApplet.runSketch( a, new Gant());
     }
     
     public static void main(String[] arg)
     {
         Main main = new Main();
-		main.cafe();        
+		main.gant();        
     }
 }
